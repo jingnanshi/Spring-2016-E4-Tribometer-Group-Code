f2417e71fae995acd9822fe63f5b844cf19a687d
Jingnan Shi <jshi@g.hmc.edu>
2016-05-02 13:30:56 -0700

    added guy components

--- Python_UI/src/tribo.py
+++ Python_UI/src/tribo.py
@@ -18,7 +18,8 @@ class App:
 
         self.dist = 0 # positive corresponds to left
         self.moveAmount = 1
-
+        # initialize arduino port
+        self.arduinoPort = ArduinoControl.getArduinoPort()
 
         self.master = master
 
@@ -28,16 +29,21 @@ class App:
         # rpm and position labels
         Label(master, text="RPM").grid(row=1, sticky=W)
         Label(master, text="Distance from Center").grid(row=2, sticky=W)
-        # rpm and dist labels
+        Label(master, text="Revolutions to rotate").grid(row=3, sticky=W)
+        # rpm and dist and revs labels
         e_rpm = Entry(master)
         e_dist = Entry(master)
+        e_revs = Entry(master)
         self.e_rpm = e_rpm
         self.e_dist = e_dist
-        # rpm and dist go buttons
+        self.e_revs = e_revs
+        # rpm and dist and revs go buttons
         rpm_go = Button(master, text="Go",
                         command=self.sendRPM,padx=3,pady=2,width=5)
         dist_go = Button(master, text="Go",
-                         command=self.sendPOS,padx=3,pady=2,width=5)    
+                         command=self.sendPOS,padx=3,pady=2,width=5)
+        revs_go = Button(master, text="Go",
+                         command=self.sendPOS,padx=3,pady=2,width=5)
          
         # Calibrate button
         cali = Button(master,text="Calibrate zero",command=self.calibrate)
@@ -52,9 +58,11 @@ class App:
         rpm_go.grid(row=1, column=2)
         e_dist.grid(row=2, column=1)
         dist_go.grid(row=2,column=2)
-        cali.grid(row=3,column=2)
-        stepper_left.grid(row=3,column=0)
-        stepper_right.grid(row=3,column=1)
+        e_revs.grid(row=3,column=1)
+        revs_go.grid(row=3,column=2)
+        cali.grid(row=3,column=3)
+        stepper_left.grid(row=4,column=0)
+        stepper_right.grid(row=4,column=1)
 
          # menu
         menubar = Menu(master)
@@ -104,6 +112,12 @@ class App:
             tkMessageBox.showwarning("Invalid Value","Not an Integer")
             return -1
 
+    def sendREVS(self):
+        """ send revolution contents
+        """
+        pass
+
+
 
     def stepperToLeft(self):
         """ stepper to the left
@@ -125,8 +139,6 @@ class App:
         tkMessageBox.showinfo("About", """Tribometer Group E4 Section 3 Spring
                               2016, contact Jingnan Shi for questions -
                               jshi@g.hmc.edu""")
-
-
 
 
 
